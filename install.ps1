b2830dbeb1e90b8de255ff9c1c9f1a9aa99b698d
zhiyucn <zhiyuxl@outlook.com>
2025-06-22 14:22:06 +0800

    fix(install): 修复安装脚本中的用户输入问题
    将Linux风格的`read -n 1 -s`命令替换为PowerShell原生命令`Read-Host`，确保在Windows环境下能正常接收用户输入

--- install.ps1
+++ install.ps1
@@ -1,6 +1,7 @@
+chcp 65001
 echo "开始安装QBotX"
 echo "按任意键继续"
-read -n 1 -s
+Read-Host -Prompt "Press Enter to continue"
 # 克隆QBotX仓库到本地
 git clone https://github.com/zhiyucn/QBotX.git
 # 安装uv管理器
@@ -13,4 +14,4 @@ echo "QBotX 已经安装完毕！"
 echo "请配置config.toml文件"
 echo "配置完成后，你可以使用run.bat文件启动QBotX"
 echo "按任意键继续"
-read -n 1 -s
+Read-Host -Prompt "Press Enter to continue"
